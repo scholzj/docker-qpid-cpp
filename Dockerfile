1236d4b199ee181b04004bf217eada7ff48ce7d2
Jakub Scholz <jakub@scholz.cz>
2016-03-27 23:51:08 +0200

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+FROM		centos:centos7
+MAINTAINER 	JAkub Scholz "www@scholzj.com"
+
+# Add qpidd group / user
+RUN groupadd -r qpidd && useradd -r -d /var/lib/qpidd -m -g qpidd qpidd
+
+# Install all dependencies
+RUN yum -y install epel-release \
+        && yum -y --setopt=tsflag=nodocs install openssl cyrus-sasl cyrus-sasl-md5 qpid-cpp-server qpid-cpp-server-linearstore qpid-cpp-server-xml qpid-tools qpid-proton-c \
+        && yum clean all
+
+ENV QPIDD_VERSION 0.34
+
+VOLUME /var/lib/qpidd
+
+# Add entrypoint
+COPY ./docker-entrypoint.sh /
+ENTRYPOINT ["/docker-entrypoint.sh"]
+
+USER qpidd:qpidd
+
+# Expose port and run
+EXPOSE 5671 5672
+CMD ["qpidd"]
---
+++ docker-entrypoint.sh
@@ -0,0 +1,213 @@
+#!/bin/bash
+set -e
+
+# if command starts with an option, prepend qpidd
+if [ "${1:0:1}" = '-' ]; then
+    set -- qpidd "$@"
+fi
+
+if [ "$1" = "qpidd" ]; then
+    sasl_external=0
+    sasl_plain=0
+    need_config=0
+
+    # Home dir
+    if [ -z "$QPIDD_HOME" ]; then
+        QPIDD_HOME="/var/lib/qpidd"
+    fi
+
+    if [ ! -d "$QPIDD_HOME" ]; then
+        mkdir -p "$QPIDD_HOME"
+        chown -R qpidd:qpidd "$QPIDD_HOME"
+    fi
+
+    #####
+    # If SASL database already exists, change the password only when it was provided from outside.
+    # If it doesn't exist, create it either with password from env or with default password
+    #####
+    if [ -z "$QPIDD_SASL_DB"]; then
+        QPIDD_SASL_DB="/var/lib/qpidd/etc/sasl/qpidd.sasldb"
+    fi
+    
+    mkdir -p "$(dirname $QPIDD_SASL_DB)"
+
+    if [[ "$QPIDD_ADMIN_USERNAME" && "$QPIDD_ADMIN_PASSWORD" ]]; then
+        echo "SASLDB"
+        echo "$QPIDD_ADMIN_PASSWORD" | saslpasswd2 -f "$QPIDD_SASL_DB" -u QPID -p "$QPIDD_ADMIN_USERNAME"
+        sasl_plain=1
+        need_config=1
+    fi
+
+    #####
+    # SSL
+    #####
+    if [[ "$QPIDD_SSL_SERVER_PUBLIC_KEY" && "$QPIDD_SSL_SERVER_PRIVATE_KEY" ]]; then
+        echo "SSL"
+        tempDir="$(mktemp -d)"
+        
+        if [ -z "$QPIDD_SSL_DB_DIR" ]; then
+            QPIDD_SSL_DB_DIR="/var/lib/qpidd/etc/ssl"
+        fi
+
+        mkdir -p "$(dirname $QPIDD_SSL_DB_DIR)"
+
+        if [ -z "$QPIDD_SSL_DB_PASSWORD" ]; then
+            QPIDD_SSL_DB_PASSWORD="$(date +%s | sha256sum | base64 | head -c 32 ; echo)"
+        fi
+
+        # Password file
+        echo "$QPIDD_SSL_DB_PASSWORD" > $QPIDD_SSL_DB_DIR/pwdfile
+        QPIDD_SSL_DB_PASSWORD_FILE="$QPIDD_SSL_DB_DIR/pwdfile"
+
+        # Server key
+        echo "$QPIDD_SSL_SERVER_PUBLIC_KEY" > $tempDir/serverKey.crt
+        echo "$QPIDD_SSL_SERVER_PRIVATE_KEY" > $tempDir/serverKey.pem
+        openssl pkcs12 -export -in $tempDir/serverKey.crt -inkey $tempDir/serverKey.pem -out $tempDir/serverKey.p12 -passout $QPIDD_SSL_DB_PASSWORD_FILE
+
+        # Does the database already exist?
+        certutil -L -d sql:$QPIDD_SSL_DB_DIR >> /dev/null
+        if [ $?=0] ; then
+            certutil -N -d sql:$QPIDD_SSL_DB_DIR -f $QPIDD_SSL_DB_PASSWORD_FILE
+        fi
+
+        # Delete old server keys
+        certutil -L -d sql:$QPIDD_SSL_DB_DIR -n serverKey
+        res=$?
+        while [ $res=0 ]; do
+            certutil -D -d sql:$QPIDD_SSL_DB_DIR -n serverKey
+            res=$?
+        done
+
+        # Load server certificate
+        certutil -A -d sql:$QPIDD_SSL_DB_DIR -n serverKey -t ",," -i $tempDir/serverKey.crt -f $QPIDD_SSL_DB_PASSWORD_FILE
+        pk12util -i $tempDir/serverKey.p12 -d sql:$QPIDD_SSL_DB_DIR -w $QPIDD_SSL_DB_PASSWORD_FILE -k $QPIDD_SSL_DB_PASSWORD_FILE
+        
+        if [ "$QPIDD_SSL_TRUSTED_CA" ]; then
+             pushd $tempDir
+             echo "$QPIDD_SSL_TRUSTED_CA" > db.ca
+             csplit db.ca '/^-----END CERTIFICATE-----$/1' '{*}' --elide-empty-files --silent --prefix=ca_
+             
+             counter=1
+             for cert in $(ls ca_*); do
+                 certutil -A -d sql:$QPIDD_SSL_DB_DIR -f $QPIDD_SSL_DB_PASSWORD_FILE -t "T,," -i $cert -n ca_$counter
+                 let counter=$counter+1
+             done
+
+             rm ca_*
+             rm db.ca
+             popd
+             
+             sasl_external=1
+        fi 
+
+        if [ "$QPIDD_SSL_TRUSTED_PEER" ]; then
+             pushd $tempDir
+             echo "$QPIDD_SSL_TRUSTED_PEER" > db.peer
+             csplit db.peer '/^-----END CERTIFICATE-----$/1' '{*}' --elide-empty-files --silent --prefix=peer_
+             
+             counter=1
+             for cert in $(ls peer_*); do
+                 certutil -A -d sql:$QPIDD_SSL_DB_DIR -f $QPIDD_SSL_DB_PASSWORD_FILE -t "P,," -i $cert -n peer_$counter
+                 let counter=$counter+1
+             done
+
+             rm peer_*
+             rm db.peer
+             popd
+
+             sasl_external=1
+        fi 
+
+        need_config=1
+    fi
+
+    #####
+    # Create SASL config if it doesn't exist, create it
+    #]####
+    if [ -z "$QPIDD_SASL_CONFIG_DIR" ]; then
+        QPIDD_SASL_CONFIG_DIR="$QPIDD_HOME/etc/sasl/"
+    fi
+    
+    if [ ! -f "$QPIDD_SASL_CONFIG_DIR/qpidd.conf" ]; then
+        echo "SASL1"
+        echo "$sasl_plain $sasl_external"
+        if [[ $sasl_plain -eq 1 || $sasl_external -eq 1 ]]; then
+            echo "SASL2"
+            mkdir -p "$(dirname $QPIDD_SASL_CONFIG_DIR)"
+        
+            mechs=""
+    
+            if [ $sasl_plain -eq 1 ]; then
+                mechs="DIGEST-MD5 CRAM-MD5 $mechs"
+            fi
+        
+            if [ $sasl_external -eq 1 ]; then
+                mechs="EXTERNAL $mechs"
+            fi
+    
+            cat > $QPIDD_SASL_CONFIG_DIR/qpidd.conf <<-EOS
+mech_list: $mechs
+pwcheck_method: auxprop
+auxprop_plugin: sasldb
+sasldb_path: $QPIDD_SASL_DB
+sql_select: dummy select
+EOS
+            need_config=1
+        fi
+    fi
+
+    #####
+    # Create ACL file - if user was set and the ACL env var not, generate it.
+    #####
+    if [ -z "$QPIDD_ACL_FILE" ]; then
+        QPIDD_ACL_FILE="$QPIDD_HOME/etc/qpidd.acl"
+    fi
+
+    if [ "$QPIDD_ACL_RULES" ]; then
+        echo "ACL passed"
+        echo $QPIDD_ACL_RULES > $QPIDD_ACL_FILE
+        need_config=1
+    elif [ $QPIDD_ADMIN_USERNAME ]; then
+        echo "ACL generated"
+        if [ ! -f "$QPIDD_ACL_FILE" ]; then
+            cat > $QPIDD_ACL_FILE <<-EOS
+acl allow $QPIDD_ADMIN_USERNAME@QPID all
+acl deny-log all all
+EOS
+        need_config=1
+        fi
+    fi
+
+    #####
+    # Generate broker config file if it doesn`t exist
+    #####
+    if [ $need_config -eq 1 ]; then
+        echo "CONFIG" 
+        if [ -z "$QPIDD_CONFIG_FILE" ]; then
+            QPIDD_CONFIG_FILE="$QPIDD_HOME/etc/qpidd.conf"
+        fi
+
+        if [ "$QPIDD_CONFIG_OPTIONS" ]; then
+            echo $QPIDD_CONFIG_OPTIONS > $QPIDD_CONFIG_FILE
+        else
+            if [ ! -f "$QPIDD_CONFIG_FILE" ]; then
+                cat > $QPIDD_CONFIG_FILE <<-EOS
+acl-file=$QPIDD_ACL_FILE
+sasl-config=$QPIDD_SASL_CONFIG_DIR
+ssl-cert-password-file=$QPIDD_SSL_DB_PASSWORD_FILE
+ssl-cert-name=serverKey
+ssl-cert-db=sql:$QPIDD_SSL_DB_DIR
+EOS
+            fi
+        fi
+
+        echo "$@"
+        set -- "$@" "--config" "$QPIDD_CONFIG_FILE"
+        echo "$@"
+    fi
+
+    chown -R qpidd:qpidd "$QPIDD_HOME"
+fi
+
+# else default to run whatever the user wanted like "bash"
+exec "$@"
